fab060ab38c7635171c9a1a2c85be98d047cb483
GPlatten <32270079+GPlatten@users.noreply.github.com>
2023-07-11 15:37:51 +0100

    Add apply sdc function

--- functions/functions.R
+++ functions/functions.R
@@ -898,3 +898,35 @@ get_download_button <-
     
     return(dt)
   }
+
+### Statistical Disclosure Control
+
+apply_sdc <-
+  function(data,
+           level = 5,
+           rounding = TRUE,
+           round_val = 5,
+           mask = "") {
+    `%>%` <- magrittr::`%>%`
+    
+    rnd <- round_val
+    
+    if (is.character(mask)) {
+      type <- function(x)
+        as.character(x)
+    } else {
+      type <- function(x)
+        x
+    }
+    
+    data %>% dplyr::mutate(dplyr::across(
+      where(is.numeric),
+      .fns = ~ dplyr::case_when(
+        .x >= level & rounding == T ~ type(rnd * round(.x / rnd)),
+        .x < level & .x > 0 & rounding == T ~ mask,
+        .x < level & .x > 0 & rounding == F ~ mask,
+        TRUE ~ type(.x)
+      ),
+      .names = "sdc_{.col}"
+    ))
+  }
